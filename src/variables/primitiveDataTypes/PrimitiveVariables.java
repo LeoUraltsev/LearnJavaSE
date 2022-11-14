65838278ae763019335d3a976dcb2f68bfc5ceb8
Leo <LeoUtaltsev@gmail.com>
2022-11-14 20:26:50 +0400

    Разбор примитивных типов данных в java.

--- src/variables/primitiveDataTypes/PrimitiveVariables.java
+++ src/variables/primitiveDataTypes/PrimitiveVariables.java
@@ -1,4 +1,48 @@
 package variables.primitiveDataTypes;
 
+/**
+ * Знакомства с примитивными типами данных в java
+ */
 public class PrimitiveVariables {
+
+    public static void main(String[] args) {
+        //Целочисленные типы данных
+        int intNumber = 321_233; // 32 бит
+        long longNumber = 1_123_456_678_890L; // 64 бит
+        short shortNumber = 1_123; // 16 бит
+        byte byteNumber = 13; // 8 бит
+
+        //Вещественные типы данных
+        double doubleNumber = 123456123123.12345678900000; //64 бит
+        float floatNumber = 12345678.12234f;
+
+        //Логичский тип данных
+        boolean booleanVariable = true; // может хранить true/false или 0/1
+
+        //Символьный тип данных
+        char charVariable = 'a'; //Хранит символы UTF-16 буквы и цифры
+        char charVariableUTF = 126; //Вывод символа '~'
+
+        System.out.println("Типы данныв в java: \n\n"
+                + "Int = "
+                + intNumber
+                + "\n\nLong = "
+                + longNumber
+                + "\n\nShort = "
+                + shortNumber
+                + "\n\nbyteNumber = "
+                + byteNumber
+                + "\n\nDouble = "
+                + doubleNumber
+                + "\n\nFloat = "
+                + floatNumber
+                + "\n\nBoolean = "
+                + booleanVariable
+                + "\n\nChar = "
+                + charVariable
+                + "\n\nChar = "
+                + charVariableUTF
+        );
+    }
+
 }
